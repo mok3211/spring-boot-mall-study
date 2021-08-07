8d070ab8f6e2e1dcea84d001d98ff5d4ec5c9857
zhangchi <zhangchi@letsflytech.com>
2021-08-07 18:21:59 +0800

    init my study project

---
+++ src/main/java/com/zhangchi/springbootmallstudy/SpringBootMallStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zhangchi.springbootmallstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMallStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMallStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhangchi/springbootmallstudy/SpringBootMallStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangchi.springbootmallstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMallStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
